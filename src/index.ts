5cc5598120c8ae5f0ab359fd5ad65f2963ec8ed2
Som Shekhar Mukherjee <iamssmkhrj@gmail.com>
2025-01-03 22:44:46 +0530

    feat: fill twoslash comments

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,50 @@
-import type { PlaygroundPlugin, PluginUtils } from "./vendor/playground"
+import type { PlaygroundPlugin, PluginUtils } from "./vendor/playground";
+import { fillTwoSlashQueries } from "./utils";
 
 const makePlugin = (utils: PluginUtils) => {
   const customPlugin: PlaygroundPlugin = {
-    id: "example",
-    displayName: "Dev Example",
+    id: "shareable-twoslash-comments",
+    displayName: "Shareable Twoslash Comments",
     didMount: (sandbox, container) => {
-      console.log("Showing new plugin")
-
       // Create a design system object to handle
       // making DOM elements which fit the playground (and handle mobile/light/dark etc)
-      const ds = utils.createDesignSystem(container)
+      const ds = utils.createDesignSystem(container);
 
-      ds.title("Example Plugin")
-      ds.p("This plugin has a button which changes the text in the editor, click below to test it")
+      ds.p(
+        "This plugin embeds twoslash (// ^?) type hints as literal comments in your code, making them easy to copy and share.",
+      );
 
-      const startButton = document.createElement("input")
-      startButton.type = "button"
-      startButton.value = "Change the code in the editor"
-      container.appendChild(startButton)
-
-      startButton.onclick = () => {
-        sandbox.setText("// You clicked the button!")
-      }
+      ds.showOptionList(
+        [
+          {
+            blurb: "Preserve multiline types instead of collapsing them to a single line.",
+            flag: "shareable-twoslash-comments/enable-multiline-comments",
+            display: "Enable multiline comments",
+            onchange: () => fillTwoSlashQueries(sandbox),
+          },
+        ],
+        {
+          style: "separated",
+        },
+      );
     },
 
     // This is called occasionally as text changes in monaco,
     // it does not directly map 1 keyup to once run of the function
     // because it is intentionally called at most once every 0.3 seconds
     // and then will always run at the end.
-    modelChangedDebounce: async (_sandbox, _model) => {
-      // Do some work with the new text
+    modelChangedDebounce: async (sandbox, model) => {
+      fillTwoSlashQueries(sandbox);
     },
 
     // Gives you a chance to remove anything set up,
     // the container itself if wiped of children after this.
     didUnmount: () => {
-      console.log("De-focusing plugin")
+      console.log("De-focusing plugin");
     },
-  }
+  };
 
-  return customPlugin
-}
+  return customPlugin;
+};
 
-export default makePlugin
+export default makePlugin;
---
+++ src/utils.ts
@@ -0,0 +1,89 @@
+import { Sandbox } from "./vendor/sandbox";
+
+const twoSlashQueryRegex = /(^\s*)(\/\/\s*\^\?)/gm;
+
+export const fillTwoSlashQueries = async (sandbox: Sandbox): Promise<void> => {
+  const multilineEnabled =
+    localStorage.getItem("shareable-twoslash-comments/enable-multiline-comments") === "true";
+  const model = sandbox.getModel();
+  const worker = await sandbox.getWorkerProcess();
+  const text = model.getValue();
+  const editOperations: import("monaco-editor").editor.IIdentifiedSingleEditOperation[] = [];
+
+  for (const match of Array.from(text.matchAll(twoSlashQueryRegex))) {
+    const commentPrefix = `${match[1]}//`.padEnd(match[0].length + 1);
+
+    const caretOffset = match.index + match[0].length - 1;
+    const caretPos = model.getPositionAt(caretOffset);
+
+    const quickInfoPos = new sandbox.monaco.Position(caretPos.lineNumber - 1, caretPos.column);
+    const quickInfoOffset = model.getOffsetAt(quickInfoPos);
+
+    const quickInfo = await worker.getQuickInfoAtPosition(
+      "file://" + model.uri.path,
+      quickInfoOffset,
+    );
+
+    if (!quickInfo?.displayParts) {
+      continue;
+    }
+
+    const quickInfoString = quickInfo.displayParts.map((d) => d.text).join("");
+
+    const quickInfoComment = `${match[0]} ${
+      multilineEnabled
+        ? quickInfoString.replace(/\n/g, `\n${commentPrefix}`)
+        : quickInfoString.replace(/\r?\n\s*/g, " ")
+    }`;
+
+    const prevQuickInfoComment = getPreviousQuickInfoComment({
+      model,
+      lineNumber: caretPos.lineNumber,
+      commentPrefix,
+    });
+    const prevQuickInfoLines = prevQuickInfoComment.split("\n").length;
+    const prevQuickInfoEndLine = caretPos.lineNumber + prevQuickInfoLines - 1;
+
+    if (prevQuickInfoComment !== quickInfoComment) {
+      editOperations.push({
+        range: new sandbox.monaco.Range(
+          caretPos.lineNumber,
+          0,
+          prevQuickInfoEndLine,
+          model.getLineContent(prevQuickInfoEndLine).length + 1,
+        ),
+        text: quickInfoComment,
+      });
+    }
+  }
+
+  if (editOperations.length > 0) {
+    model.pushEditOperations([], editOperations, () => null);
+  }
+};
+
+type GetPreviousQuickInfo = (args: {
+  model: import("monaco-editor").editor.ITextModel;
+  lineNumber: number;
+  commentPrefix: string;
+}) => string;
+
+const getPreviousQuickInfoComment: GetPreviousQuickInfo = ({
+  model,
+  lineNumber,
+  commentPrefix,
+}) => {
+  const prevQuickInfoLines: string[] = [];
+
+  for (let currLineNumber = lineNumber; currLineNumber <= model.getLineCount(); currLineNumber++) {
+    const lineContent = model.getLineContent(currLineNumber);
+
+    if (!(lineContent.startsWith(commentPrefix) || lineContent.search(twoSlashQueryRegex) !== -1)) {
+      break;
+    }
+
+    prevQuickInfoLines.push(lineContent);
+  }
+
+  return prevQuickInfoLines.join("\n");
+};
